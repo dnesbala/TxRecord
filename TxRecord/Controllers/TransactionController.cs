c3c461cd9a83b339adf5acb37b27b0c491f5b552
Dinesh Bala <baladnes@gmail.com>
2021-09-23 17:25:43 +0545

    Initial setup, added homepage

---
+++ TxRecord/Controllers/TransactionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TxRecord.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Insight()
+        {
+            return View();
+        }
+    }
+}
